b762b8dc189ff794f9f3c2b518a35d85ec46bd70
Zane Helton <zane99@me.com>
2023-04-12 18:47:46 -0400

    Add warning icon if donations are paused

--- src/components/Donations/DonationPanel.js
+++ src/components/Donations/DonationPanel.js
@@ -1,11 +1,15 @@
-import { Box, Flex } from '@mantine/core'
+import { Box, Flex, Text, Tooltip } from '@mantine/core'
 import { useMediaQuery } from '@mantine/hooks'
+import { ReactComponent as IconWarning } from '../../warning-icon.svg'
 import { DonationLog } from '.'
 import { PanelHead } from '../document'
 import BlankAudio from './BlankAudio'
+import { useContext } from 'react'
+import { DonationContext } from 'components/Providers/DonationProvider'
 
 const DonationPanel = () => {
   const isLarge = useMediaQuery('(min-width: 1440px)')
+  const { isAutoplay } = useContext(DonationContext)
 
   return (
     <Flex direction="column" h="100%" style={{ alignSelf: 'stretch' }}>
@@ -15,6 +19,28 @@ const DonationPanel = () => {
         }}
       >
         Donations
+        {!isAutoplay && (
+          <Tooltip
+            position="right"
+            withinPortal
+            label={
+              <Box m="sm">
+                <Text>Donations are paused.</Text>
+              </Box>
+            }
+          >
+            <span>
+              <IconWarning
+                style={{
+                  verticalAlign: 'middle',
+                  marginLeft: '12px',
+                  width: '24px',
+                  height: '24px',
+                }}
+              />
+            </span>
+          </Tooltip>
+        )}
       </PanelHead>
       <Box
         style={{
