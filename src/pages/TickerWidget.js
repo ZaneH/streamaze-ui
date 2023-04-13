4ecc3a67975d4261a76235eeeabd9a4875f8d6ca
Zane Helton <zane99@me.com>
2023-04-13 15:26:17 -0400

    Add donations to ticker

--- src/pages/TickerWidget.js
+++ src/pages/TickerWidget.js
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled'
-import { useEffect, useState } from 'react'
+import { Space } from '@mantine/core'
+import { useInterval } from '@mantine/hooks'
+import { DonationContext } from 'components/Providers/DonationProvider'
+import { useContext, useEffect, useState } from 'react'
 import Marquee from 'react-fast-marquee'
 import { useParams } from 'react-router-dom'
 import { useLanyardWS } from 'use-lanyard'
@@ -17,8 +20,31 @@ const TickerWidget = () => {
   const { id } = useParams()
   const data = useLanyardWS(id)
   const kv = data?.kv
-
+  const { donations } = useContext(DonationContext)
+  const [visibleDonations, setVisibleDonations] = useState([])
   const [tickerData, setTickerData] = useState(null)
+
+  useEffect(() => {
+    setVisibleDonations((prev) => [donations[donations.length - 1], ...prev])
+    if (!donoInterval.active) {
+      donoInterval.start()
+    }
+  }, [donations, setVisibleDonations])
+
+  const donoInterval = useInterval(() => {
+    if (visibleDonations && visibleDonations.length > 0) {
+      setVisibleDonations((prev) => prev.slice(0, prev.length - 1))
+    } else {
+      donoInterval.stop()
+      setVisibleDonations([])
+    }
+  }, 6000)
+
+  useEffect(() => {
+    donoInterval.start()
+    return donoInterval.stop
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     try {
@@ -36,10 +62,39 @@ const TickerWidget = () => {
     return null
   }
 
+  const visibleDonation = visibleDonations?.[0]
+  const { type } = visibleDonation || {}
+  const { metadata } = visibleDonation?.data || {}
+  const isKickSubscription = type === 'kick_subscription'
+  const isKickGift = type === 'kick_gifted_subscription'
+
   return (
     <TickerContainer>
       <Marquee gradient={false} speed={80}>
-        {tickerData}
+        {visibleDonation ? (
+          <span>
+            {visibleDonation?.data?.name}{' '}
+            {type === 'membershipGift' &&
+              `${metadata?.gift_count}x ${metadata?.gift_level}s`}
+            {type === 'subscription' && metadata
+              ? `became a member for ${metadata.months} month${
+                  metadata.months > 1 ? 's' : ''
+                }`
+              : (type === 'superchat' || type === 'donation') &&
+                'donated to the stream'}
+            {isKickSubscription &&
+              `subscribed on Kick for ${metadata.months} month${
+                metadata.months > 1 ? 's' : ''
+              }`}
+            {isKickGift &&
+              `gifted ${metadata?.months} subscription${
+                metadata?.months > 1 ? 's' : ''
+              } on Kick`}
+          </span>
+        ) : (
+          tickerData
+        )}
+        &nbsp;&nbsp;&nbsp;
       </Marquee>
     </TickerContainer>
   )
