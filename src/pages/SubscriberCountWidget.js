c13ebd148ca217d5cc9866cc68c7b5132e6f9ec6
Zane Helton <zane99@me.com>
2023-05-11 13:55:03 -0400

    Update sub goal

--- src/pages/SubscriberCountWidget.js
+++ src/pages/SubscriberCountWidget.js
@@ -36,7 +36,7 @@ const SubscriberCountWidget = () => {
   return (
     <NumberWidget>
       <b>
-        Subs: {isAll ? `${allSubs + totalOffset} / 1500` : null}
+        Subs: {isAll ? `${allSubs + totalOffset} / 2000` : null}
         {isKick ? kickSubs + kickOffset : null}
         {isYouTube ? youtubeSubs + youtubeSubscriptionOffset : null}
       </b>
