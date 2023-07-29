1990b4bb93304f9d120a6cb5df2cb409bb6fbab0
Zane Helton <zane99@me.com>
2023-07-29 00:03:41 -0400

    Add Lanyard Provider to PP

--- src/components/Providers/ProviderProvider.js
+++ src/components/Providers/ProviderProvider.js
@@ -1,20 +1,23 @@
 import DonationProvider from './DonationProvider'
 import HopProvider from './HopProvider'
+import LanyardProvider from './LanyardProvider'
 import PhoenixProvider from './PhoenixProvider'
 import StatProvider from './StatProvider'
 import SubathonProvider from './SubathonProvider'
 
 const ProviderProvider = ({ children }) => {
   return (
-    <DonationProvider>
-      <HopProvider>
-        <SubathonProvider>
-          <StatProvider>
-            <PhoenixProvider>{children}</PhoenixProvider>
-          </StatProvider>
-        </SubathonProvider>
-      </HopProvider>
-    </DonationProvider>
+    <LanyardProvider>
+      <DonationProvider>
+        <HopProvider>
+          <SubathonProvider>
+            <StatProvider>
+              <PhoenixProvider>{children}</PhoenixProvider>
+            </StatProvider>
+          </SubathonProvider>
+        </HopProvider>
+      </DonationProvider>
+    </LanyardProvider>
   )
 }
 
