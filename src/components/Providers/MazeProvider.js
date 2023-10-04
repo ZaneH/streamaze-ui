1532b3ee0e8146c8ff5b69793f16516d3704d040
Zane Helton <zane99@me.com>
2023-10-04 13:19:02 -0400

    Fix error on maze widget

--- src/components/Providers/MazeProvider.js
+++ src/components/Providers/MazeProvider.js
@@ -22,7 +22,7 @@ const DIRECTIONS = {
 
 const MazeProvider = ({ children, isController }) => {
   const { kv, updateKV } = useContext(LanyardContext)
-  const { isLive, bitrate } = useContext(HopContext)
+  const { isLive, bitrate } = useContext(HopContext) || {}
   const isMazeEnabled = kv?.maze_enabled === 'true'
   const [cursorIdx, setCursorIdx] = useState(0)
   const [size, setSize] = useState({
